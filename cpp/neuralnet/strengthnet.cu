1441136e8ad21c181d06d3095cbd2c0a4c3adebe
Peter Neubauer <peterneubauer2@gmail.com>
2023-12-06 12:18:35 +0100

    Strengthnet: unit test for softmaxDerivative, separate UTs
    Also add namespace StrengthNetKernels

--- cpp/neuralnet/strengthnet.cu
+++ cpp/neuralnet/strengthnet.cu
@@ -15,6 +15,9 @@ constexpr dim3 numBlocksForTensor(const Tensor& t, dim3 blockDim) noexcept {
 
 }
 
+namespace StrengthNetKernels
+{
+
 // 1 block with N threads
 __global__ void scale(Tensor y, const Tensor w) {
   y.data[threadIdx.x] *= w.data[0];
@@ -98,8 +101,8 @@ __device__ float max(const Tensor& a) {
 
   uint n = a.dims.x;
   for (uint s = 1; s < n; s *= 2) {
-      if (threadIdx.x + s < n) {
-          buffer[threadIdx.x] = max(buffer[threadIdx.x], buffer[threadIdx.x + s]);
+      if (threadIdx.x + s < n && buffer[threadIdx.x] < buffer[threadIdx.x + s]) {
+          buffer[threadIdx.x] = buffer[threadIdx.x + s];
       }
       __syncthreads();
   }
@@ -174,9 +177,6 @@ __global__ void update(Tensor W, const Tensor W_grad, float learnrate) {
   W.data[threadIdx.x * W.dims.y + threadIdx.y] -= W_grad.data[threadIdx.x * W_grad.dims.y + threadIdx.y] * learnrate;
 }
 
-
-
-
 // __global__ void forwardTanhKernel(float* softx, int ch, int row) {
 //   softx[threadIdx.x * ch + row] = 10.f * tanhf(softx[threadIdx.x * ch + row]);
 // }
@@ -186,6 +186,9 @@ __global__ void update(Tensor W, const Tensor W_grad, float learnrate) {
 //   outgrads[threadIdx.x*2 + 1] = ingrads[threadIdx.x*2 + 1] * 10.f / (cosa*cosa);
 // }
 
+} // end namespace StrengthNetKernels
+
+using namespace StrengthNetKernels;
 
 void StrengthNet::forward() {
   uint N = x.dims.x;
@@ -205,21 +208,6 @@ void StrengthNet::forward() {
   // aggregate by attention
   softmax<<<1, N, N*sizeof(float)>>>(a);
   dotproduct<<<1, N>>>(y, r, a);
-
-  // assert(input.dims.x * dims.y <= maxN);
-  // assert(reinterpret_cast<const float*>(&input[0]) + in_ch == reinterpret_cast<const float*>(&input[1])); // crude packing/alignment safety check
-  // N = input.size();
-  // const float* inputPtr = reinterpret_cast<const float*>(input.data());
-  // CUDA_ERR("StrengthNet.forward", cudaMemcpy(inputx, inputPtr, N * in_ch * sizeof(float), cudaMemcpyHostToDevice));
-
-  // forwardFull(inputx, hiddenx, Wh, bh, N, in_ch, hidden_ch);
-  // forwardRelu(hiddenx, N, hidden_ch);
-  // forwardFull(hiddenx, outputx, Wo, bo, N, hidden_ch, out_ch);
-  // forwardAggregate(outputx, softx, aggregx, N, out_ch);
-
-  // StrengthNet::Output result;
-  // CUDA_ERR("StrengthNet.forward", cudaMemcpy(&result, aggregx, sizeof(StrengthNet::Output), cudaMemcpyDeviceToHost));
-  // return result;
 }
 
 void StrengthNet::backward(float target, float learnrate) {
@@ -259,166 +247,5 @@ void StrengthNet::backward(float target, float learnrate) {
   update<<<1, {W1.dims.x, W1.dims.y}>>>(W1, W1_grad, learnrate);
   update<<<1, {W2r.dims.x, W2r.dims.y}>>>(W2r, W2r_grad, learnrate);
   update<<<1, {W2z.dims.x, W2z.dims.y}>>>(W2z, W2z_grad, learnrate);
-
-  // CUDA_ERR("StrengthNet.backward", cudaMemcpy(back1x, &target, sizeof(StrengthNet::Output), cudaMemcpyHostToDevice));
-  // // dumpDeviceArray("inputx", inputx, N, in_ch);
-  // // dumpDeviceArray("hiddenx", hiddenx, N, hidden_ch);
-  // // dumpDeviceArray("outputx", outputx, N, out_ch);
-  // // dumpDeviceArray("softx", softx, N, out_ch);
-  // // dumpDeviceArray("aggregx", aggregx, 1, 1);
-  // backwardLoss(back1x, aggregx, back2x);
-  // dumpDeviceArray("dLoss/dAggreg", back2x, 1, 1);
-
-  // backwardAggregate(back2x, softx, N, back1x);
-  // backwardTanh(back1x, outputx, N, back2x);
-  // dumpDeviceArray("dLoss/dOutput", back2x, N, out_ch);
-  // backwardFull(back2x, hiddenx, Wo, bo, hidden_ch, out_ch, N, learnrate, back1x);
-  // dumpDeviceArray("dLoss/dHidden", back1x, N, hidden_ch);
-  // backwardRelu(back1x, hiddenx, hidden_ch, N, back2x);
-  // dumpDeviceArray("dLoss/dHidden+ReLu", back2x, N, hidden_ch);
-  // backwardFull(back2x, inputx, Wh, bh, in_ch, hidden_ch, N, learnrate, back1x);
-  // dumpDeviceArray("Wo", Wo, hidden_ch, out_ch);
-  // dumpDeviceArray("bo", bo, 1, out_ch);
-  // dumpDeviceArray("Wh", Wh, in_ch, hidden_ch);
-  // dumpDeviceArray("bh", bh, 1, hidden_ch);
 }
 
-
-// unit tests for local defined kernel functions
-
-#include <sstream>
-// static void checkCudaError(const cudaError_t status, const char* opName, const char* file, const char* func, int line) {
-//   if(status != cudaSuccess)
-//     cout << std::string("CUDA Error, for ") << opName << " file " << file << ", func " << func << ", line " << line << ", error " << cudaGetErrorString(status);
-// }
-// #define CUDA_ERR(opName,x) { checkCudaError((x),opName,__FILE__,#x,__LINE__); }
-
-using namespace std;
-
-namespace Tests {
-void runStrengthModelTests() {
-  cout << "Running strength model tests" << endl;
-  ostringstream out;
-
-  {
-    cout << "- matmul: ";
-    vector<float> A_data = {1, -3, 3, 2, -2, -1, 1, 0, -1}; // left operand, 3x3 (with bias column)
-    vector<float> B_data = {7, -10, 3, 8, -11, 4, 8, 7}; // right operand, 2x4
-    vector<float> C_data = {-12, -1, 30,  20, -25, 0,  -2, 25, -38,  23, -38, 16}; // expected result, 3x4
-    Tensor A(3, 3);
-    Tensor B(4, 2);
-    Tensor C(4, 3);
-    cudaMemcpy(A.data, A_data.data(), 3*3 * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(B.data, B_data.data(), 4*2 * sizeof(float), cudaMemcpyHostToDevice);
-
-    matmul<<<1, {16, 16}>>>(C, A, B);
-    vector<float> C_result = static_cast<vector<float>>(C);
-    bool pass = true;
-    for(size_t i = 0; i < C_data.size(); i++)
-      if(fabs(C_data[i] - C_result[i]) > 0.0001)
-        pass = false;
-
-    cout << (pass ? "pass" : "fail") << "\n";
-
-    if(!pass) {
-      Tensor C_expected(4,3);
-      cudaMemcpy(C_expected.data, C_data.data(), 4*3 * sizeof(float), cudaMemcpyHostToDevice);
-      C_expected.print(std::cout, "matmul expected");
-      C.print(std::cout, "matmul result");
-    }
-  }
-
-  {
-    cout << "- transposeMatmul: ";
-    vector<float> A_data = {1, -3, 3, 2, -2, -1}; // left operand, 3x2
-    vector<float> B_data = {7, 3, -11, 8, -10, 8, 4, 7}; // right operand, 4x2
-    vector<float> C_data = {-13, -1, 31,  19, -25, 1,  -3, 25, -37,  22, -38, 17}; // expected result, 3x4
-    Tensor A(2, 3);
-    Tensor B(2, 4);
-    Tensor C(4, 3);
-    cudaMemcpy(A.data, A_data.data(), 2*3 * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(B.data, B_data.data(), 4*2 * sizeof(float), cudaMemcpyHostToDevice);
-
-    transposeMatmul<<<1, {16, 16}>>>(C, A, B);
-    vector<float> C_result = static_cast<vector<float>>(C);
-    bool pass = true;
-    for(size_t i = 0; i < C_data.size(); i++)
-      if(fabs(C_data[i] - C_result[i]) > 0.0001)
-        pass = false;
-
-    cout << (pass ? "pass" : "fail") << "\n";
-
-    if(!pass) {
-      Tensor C_expected(4,3);
-      cudaMemcpy(C_expected.data, C_data.data(), 4*3 * sizeof(float), cudaMemcpyHostToDevice);
-      C_expected.print(std::cout, "transposeMatmul expected");
-      C.print(std::cout, "transposeMatmul result");
-    }
-  }
-
-  {
-    cout << "- softmax: ";
-    vector<float> A_data = {2.796027196f, 3.306852819f, 2.390562088f}; // operand, 3x1
-    vector<float> S_data = {.3f, .5f, .2f}; // expected result, 3x1
-    Tensor A(3, 1);
-    cudaMemcpy(A.data, A_data.data(), 3*1 * sizeof(float), cudaMemcpyHostToDevice);
-
-    // softmax<<<1, 3>>>(A);
-    softmax<<<1, 3, 3*sizeof(float)>>>(A);
-    vector<float> S_result = static_cast<vector<float>>(A);
-    bool pass = true;
-    for(size_t i = 0; i < S_data.size(); i++)
-      if(fabs(S_data[i] - S_result[i]) > 0.0001)
-        pass = false;
-
-    cout << (pass ? "pass" : "fail") << "\n";
-
-    if(!pass) {
-      Tensor A_expected(3,1);
-      cudaMemcpy(A_expected.data, S_data.data(), 3*1 * sizeof(float), cudaMemcpyHostToDevice);
-      A_expected.print(std::cout, "softmax expected");
-      A.print(std::cout, "softmax result");
-    }
-  }
-
-  {
-    cout << "- matmulDerived: ";
-    vector<float> W_data = {1, -3, 3, 2, -2, -1, 1, 0, -1}; // left operand, 3x3 (with bias column)
-    vector<float> Y_grad = {-2, -1, 3,  2, -5, 0,  -2, 5, -8,  3, -3, 1}; // dL/dy
-    vector<float> D_data = {10, -5, 17, 14, -41, -6, 15, 11}; // expected result dL/dx, 2x4
-    Tensor W(3, 3);
-    Tensor Y(4, 3);
-    Tensor DmatDx(4, 2);
-    cudaMemcpy(W.data, W_data.data(), 3*3 * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(Y.data, Y_grad.data(), 4*3 * sizeof(float), cudaMemcpyHostToDevice);
-
-    matmulDerived<<<1, {16, 16}>>>(DmatDx, Y, W);
-    vector<float> D_result = static_cast<vector<float>>(DmatDx);
-    bool pass = true;
-    for(size_t i = 0; i < D_data.size(); i++)
-      if(fabs(D_data[i] - D_result[i]) > 0.0001)
-        pass = false;
-
-    cout << (pass ? "pass" : "fail") << "\n";
-
-    if(!pass) {
-      Tensor D_expected(4,2);
-      cudaMemcpy(D_expected.data, D_data.data(), 4*2 * sizeof(float), cudaMemcpyHostToDevice);
-      D_expected.print(std::cout, "matmulDerived expected");
-      DmatDx.print(std::cout, "matmulDerived result");
-    }
-  }
-
-  {
-    // vector<float> W_data = {1, -3, 3, 2, -2, -1}; // expected result, 3x2
-    // Tensor DmatDx({3, 2});
-    // dmatmul_dx(A, DmatDx);
-    // vector<float> D_result = DmatDx;
-    // pass = true;
-    // for(size_t i = 0; i < D_data.size(); i++)
-    //   if(fabs(D_data[i] - D_result[i]) > 0.0001)
-    //     pass = false;
-    // cout << "  backward dx: " << (pass ? "pass" : "fail") << "\n";
-  }
-}
-}
---
+++ cpp/tests/teststrengthnet.cu
@@ -0,0 +1,169 @@
+// unit tests for strength net kernel functions in strengthnet.cu
+
+#include <vector>
+#include <sstream>
+#include "neuralnet/strengthnet.h"
+
+using namespace std;
+
+namespace StrengthNetKernels
+{
+
+// definitions in strengthnet.cu
+__global__ void scale(Tensor y, const Tensor w);
+__global__ void add(Tensor y, const Tensor a, const Tensor b);
+__global__ void dotproduct(Tensor y, const Tensor a, const Tensor b);
+__global__ void matmul(Tensor y, const Tensor W, const Tensor x);
+__global__ void transposeMatmul(Tensor y, const Tensor a, const Tensor b);
+__global__ void relu(Tensor h);
+__global__ void softmax(Tensor a);
+__global__ void lossDerived(Tensor y_grad, float target, const Tensor y);
+__global__ void softmaxDerived(Tensor z_grad, const Tensor a);
+__global__ void matmulDerived(Tensor x_grad, const Tensor y_grad, const Tensor W);
+__global__ void update(Tensor W, const Tensor W_grad, float learnrate);
+
+}
+
+using namespace StrengthNetKernels;
+
+namespace Tests {
+void runStrengthModelTests() {
+  cout << "Running strength model tests" << endl;
+
+  {
+    cout << "- matmul: ";
+    vector<float> A_data = {1, -3, 3, 2, -2, -1, 1, 0, -1}; // left operand, 3x3 (with bias column)
+    vector<float> B_data = {7, -10, 3, 8, -11, 4, 8, 7}; // right operand, 2x4
+    vector<float> C_data = {-12, -1, 30,  20, -25, 0,  -2, 25, -38,  23, -38, 16}; // expected result, 3x4
+    Tensor A(3, 3);
+    Tensor B(4, 2);
+    Tensor C(4, 3);
+    cudaMemcpy(A.data, A_data.data(), 3*3 * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(B.data, B_data.data(), 4*2 * sizeof(float), cudaMemcpyHostToDevice);
+
+    matmul<<<1, {16, 16}>>>(C, A, B);
+    vector<float> C_result = static_cast<vector<float>>(C);
+    bool pass = true;
+    for(size_t i = 0; i < C_data.size(); i++)
+      if(fabs(C_data[i] - C_result[i]) > 0.0001)
+        pass = false;
+
+    cout << (pass ? "pass" : "fail") << "\n";
+
+    if(!pass) {
+      Tensor C_expected(4,3);
+      cudaMemcpy(C_expected.data, C_data.data(), 4*3 * sizeof(float), cudaMemcpyHostToDevice);
+      C_expected.print(cout, "matmul expected");
+      C.print(cout, "matmul result");
+    }
+  }
+
+  {
+    cout << "- transposeMatmul: ";
+    vector<float> A_data = {1, -3, 3, 2, -2, -1}; // left operand, 3x2
+    vector<float> B_data = {7, 3, -11, 8, -10, 8, 4, 7}; // right operand, 4x2
+    vector<float> C_data = {-13, -1, 31,  19, -25, 1,  -3, 25, -37,  22, -38, 17}; // expected result, 3x4
+    Tensor A(2, 3);
+    Tensor B(2, 4);
+    Tensor C(4, 3);
+    cudaMemcpy(A.data, A_data.data(), 2*3 * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(B.data, B_data.data(), 4*2 * sizeof(float), cudaMemcpyHostToDevice);
+
+    transposeMatmul<<<1, {16, 16}>>>(C, A, B);
+    vector<float> C_result = static_cast<vector<float>>(C);
+    bool pass = true;
+    for(size_t i = 0; i < C_data.size(); i++)
+      if(fabs(C_data[i] - C_result[i]) > 0.0001)
+        pass = false;
+
+    cout << (pass ? "pass" : "fail") << "\n";
+
+    if(!pass) {
+      Tensor C_expected(4,3);
+      cudaMemcpy(C_expected.data, C_data.data(), 4*3 * sizeof(float), cudaMemcpyHostToDevice);
+      C_expected.print(cout, "transposeMatmul expected");
+      C.print(cout, "transposeMatmul result");
+    }
+  }
+
+  {
+    cout << "- softmax: ";
+    vector<float> A_data = {2.796027196f, 3.306852819f, 2.390562088f}; // operand, 3x1
+    vector<float> S_data = {.3f, .5f, .2f}; // expected result, 3x1
+    Tensor A(3, 1);
+    cudaMemcpy(A.data, A_data.data(), 3*1 * sizeof(float), cudaMemcpyHostToDevice);
+
+    // softmax<<<1, 3>>>(A);
+    softmax<<<1, 3, 3*sizeof(float)>>>(A);
+    vector<float> S_result = static_cast<vector<float>>(A);
+    bool pass = true;
+    for(size_t i = 0; i < S_data.size(); i++)
+      if(fabs(S_data[i] - S_result[i]) > 0.0001)
+        pass = false;
+
+    cout << (pass ? "pass" : "fail") << "\n";
+
+    if(!pass) {
+      Tensor A_expected(3,1);
+      cudaMemcpy(A_expected.data, S_data.data(), 3*1 * sizeof(float), cudaMemcpyHostToDevice);
+      A_expected.print(cout, "softmax expected");
+      A.print(cout, "softmax result");
+    }
+  }
+
+  {
+    cout << "- softmaxDerived: ";
+    vector<float> A_data = {.3f, .5f, .2f}; // softmax outputs
+    vector<float> Z_data = {1.f, 2.f, 3.f}; // gradient in & out
+    vector<float> G_data = {-.27f, .05f, .22f}; // expected gradient out
+    Tensor A(3, 1);
+    Tensor Z(3, 1);
+    cudaMemcpy(A.data, A_data.data(), 3*1 * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(Z.data, Z_data.data(), 3*1 * sizeof(float), cudaMemcpyHostToDevice);
+
+    softmaxDerived<<<1, 3>>>(Z, A);
+    vector<float> Z_result = static_cast<vector<float>>(Z);
+    bool pass = true;
+    for(size_t i = 0; i < G_data.size(); i++)
+      if(fabs(G_data[i] - Z_result[i]) > 0.0001)
+        pass = false;
+
+    cout << (pass ? "pass" : "fail") << "\n";
+    if(!pass) {
+      Tensor G_expected(3,1);
+      cudaMemcpy(G_expected.data, G_data.data(), 3*1 * sizeof(float), cudaMemcpyHostToDevice);
+      G_expected.print(cout, "softmaxDerived expected");
+      Z.print(cout, "softmaxDerived result");
+    }
+  }
+
+  {
+    cout << "- matmulDerived: ";
+    vector<float> W_data = {1, -3, 3, 2, -2, -1, 1, 0, -1}; // left operand, 3x3 (with bias column)
+    vector<float> Y_grad = {-2, -1, 3,  2, -5, 0,  -2, 5, -8,  3, -3, 1}; // dL/dy
+    vector<float> D_data = {10, -5, 17, 14, -41, -6, 15, 11}; // expected result dL/dx, 2x4
+    Tensor W(3, 3);
+    Tensor Y(4, 3);
+    Tensor DmatDx(4, 2);
+    cudaMemcpy(W.data, W_data.data(), 3*3 * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(Y.data, Y_grad.data(), 4*3 * sizeof(float), cudaMemcpyHostToDevice);
+
+    matmulDerived<<<1, {16, 16}>>>(DmatDx, Y, W);
+    vector<float> D_result = static_cast<vector<float>>(DmatDx);
+    bool pass = true;
+    for(size_t i = 0; i < D_data.size(); i++)
+      if(fabs(D_data[i] - D_result[i]) > 0.0001)
+        pass = false;
+
+    cout << (pass ? "pass" : "fail") << "\n";
+
+    if(!pass) {
+      Tensor D_expected(4,2);
+      cudaMemcpy(D_expected.data, D_data.data(), 4*2 * sizeof(float), cudaMemcpyHostToDevice);
+      D_expected.print(cout, "matmulDerived expected");
+      DmatDx.print(cout, "matmulDerived result");
+    }
+  }
+
+}
+}
